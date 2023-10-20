3000f1c9c17e06804354969bcc2f50925c467666
NuMellow <cmmukabe@gmail.com>
2023-10-20 16:54:59 -0400

    refactor glance.py

--- src/glance.py
+++ src/glance.py
@@ -3,7 +3,6 @@
 import logging
 import os
 import screen_util as screen
-import time
 import traceback
 
 from instructables_page import Instructables
@@ -11,30 +10,27 @@ from album_page import Album
 
 logging.basicConfig(level=logging.DEBUG)
 
-
-def check_instructables():
-    contests = instructables_app.update_contests()
+def start_page(app):
+    page = app()
+    page.run()
+    return page
 
 def main():
     try:
         page = 0
         if os.path.exists('glance.conf'):
-            config = open('glance.conf', 'r')
-            page = int(config.readline().split('=')[1])
-            config.close()
+            with open('glance.conf') as config:
+                row = config.readline()
+                _, value = row.split('=')
+                page = int(value)
 
         if page == 0:
-            instructables = Instructables()
-            instructables.run()
+            start_page(Instructables)
         elif page == 1:
-            album = Album()
-            album.run()
+            album = start_page(Album)
             album.download_photos()
         else:
-            instructables = Instructables()
-            instructables.run()
-
-        time.sleep(2)
+            start_page(Instructables)
     except IOError as e:
         logging.info(e)
     
