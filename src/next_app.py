0befd6af0069142d7786e566ecd4d3e71eb8b01f
NuMellow <cmmukabe@gmail.com>
2023-10-20 19:00:29 -0400

    refactor next_app script

--- src/next_app.py
+++ src/next_app.py
@@ -1,26 +1,33 @@
 import os
 
+# Default values
+cur_app = 0
+total_apps = 1
+pi_sugar=True
 
-if not os.path.exists('glance.conf'):
-    create_glance = open('glance.conf','w')
-    create_glance.write('page=0')
-    create_glance.close()
+def write_glance_config():
+    configs = [
+        f"page={cur_app}",
+        f"numofapps={total_apps}",
+        f"has_pi_sugar={pi_sugar}",
+    ]
+    with open("glance.conf", "w") as glance_file:
+        for config in configs:
+            glance_file.write(config)
+            glance_file.write("\n")
 
-glance_conf = open('glance.conf', 'r')
-cur_app = int(glance_conf.readline()[5:])
-glance_conf.close()
+if not os.path.exists("glance.conf"):
+    write_glance_config()
+else:
+    with open("glance.conf") as glance_conf:
+        for row in glance_conf:
+            key, value = row.split("=")
+            if key == "page":
+                cur_app = int(value)
+            elif key == "numofapps":
+                total_apps = int(value)
+            elif key == "has_pi_sugar":
+                pi_sugar = bool(value)
 
-if not os.path.exists('app.conf'):
-    create_app = open('app.conf','w')
-    create_app.write('numofapps=1')
-    create_app.close()
-
-app_conf = open('app.conf')
-total_apps = int(app_conf.readline().split('=')[1])
-app_conf.close()
-
-next_app = (cur_app + 1) % total_apps
-
-glance_conf = open('glance.conf', 'w')
-glance_conf.write("page=" +  str(next_app))
-glance_conf.close()
+cur_app = (cur_app + 1) % total_apps
+write_glance_config()
